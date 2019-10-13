2b3b7d6bbc21d7074d86feda973d265153c866ec
Cristian Poley <cripolgon@gmail.com>
2019-10-13 19:12:59 +0200

    [style] Added footer color

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -2,13 +2,28 @@
   <footer class="footer">
     <span
       class="footer__design-for"
+      :style="getColor()"
       v-html="$t('components.footer_app.design_for')" />
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'FooterApp'
+  name: 'FooterApp',
+  methods: {
+
+    /**
+     * Devuelve un color para el texto del footer,
+     * dependiendo de la ruta en la que se encuentra.
+     * @returns {String}
+     */
+    getColor () {
+      const color = this.$route.name === 'MoreMe' ? 'rgba(240, 231, 216, 1)' : 'rgba(14, 59, 67, 1)'
+      return {
+        '--textColor': color
+      }
+    }
+  }
 }
 </script>
 
@@ -22,7 +37,7 @@ export default {
         position: absolute;
         width: 100%;
         display: block;
-        color: #fff;
         margin-top: 3rem;
+        color: var(--textColor);
     }
 </style>
