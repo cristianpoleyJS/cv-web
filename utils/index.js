d72cf763723b9712f1b4ed185c9d29199d6a516f
Cristian Poley <cristianpoleygonzalez@gmail.com>
2021-02-14 00:01:24 +0100

    fix: changed name API KEY

--- utils/index.js
+++ utils/index.js
@@ -3,7 +3,7 @@ import Airtable from 'airtable'
 export const getDataAirtable = async (baseKey, baseName) => {
   const res = []
   await new Promise((resolve) => {
-    const base = new Airtable({ apiKey: process.env.API_KEY_AIRTABLE }).base(baseKey)
+    const base = new Airtable({ apiKey: process.env.NEXT_PUBLIC_API_KEY_AIRTABLE }).base(baseKey)
     base(baseName)
       .select()
       .firstPage((err, records) => {
